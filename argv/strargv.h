cd919773f7cbd47afaacf8a72f2a171bcf3d9527
Jin-Yang <jinyang.sia@gmail.com>
2018-08-18 00:56:33 +0800

    add strargv value macro

--- argv/strargv.h
+++ argv/strargv.h
@@ -2,6 +2,8 @@
 #ifndef _STRARGV_H_
 #define _STRARGV_H_
 
+#define strargv_value(argv, argc)   ((char *)(argv) + sizeof(char *) * (argc))
+
 int strargv(const char *str, int *argc, char ***argv, char **errmsg);
 void strargv_free(char **argv);
 
